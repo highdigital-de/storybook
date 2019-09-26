027254256247978d6e50901315a21c09e0f7fc14
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-09-26 18:19:02 +0200

    Add TileRow

---
+++ src/components/TileRow.stories.js
@@ -0,0 +1,55 @@
+import { storiesOf } from '@storybook/vue';
+import TileRow from './TileRow';
+
+storiesOf('TileRow', module)
+.add('two', () => {
+  return {
+    components: { TileRow },
+    template: `<TileRow :tiles="tiles" />`,
+    data: () => ({
+      tiles: [
+        {
+          heading: "kakao kaufen",
+          cta: "jetzt entdecken",
+          image: "/img/cacao-bg.jpg",
+        },
+        {
+          heading: "abo",
+          cta: "jetzt abonieren",
+          image: "",
+        }
+      ]
+    }),
+  };
+})
+.add('four', () => {
+  return {
+    components: { TileRow },
+    template: `<TileRow :tiles="tiles" />`,
+    data: () => ({
+      tiles: [
+        {
+          heading: "kakao kaufen",
+          cta: "jetzt entdecken",
+          image: "/img/cacao-bg.jpg",
+        },
+        {
+          heading: "abo",
+          cta: "jetzt abonieren",
+          image: "",
+        },
+        {
+          heading: "kakao kaufen",
+          cta: "jetzt entdecken",
+          image: "",
+          highlight: true
+        },
+        {
+          heading: "abo",
+          cta: "jetzt abonieren",
+          image: "/img/cacao-bg.jpg",
+        }
+      ]
+    }),
+  };
+});
--- src/components/TileRow.vue
+++ src/components/TileRow.vue
@@ -1,49 +1,36 @@
 <template>
   <div class="tileRow">
-    <Tile v-for="tile in tiles" :key="tile.heading" />
+    <div v-for="(tile, index) in tiles" :key="index" class="tileContainer">
+      <Tile v-bind="tile" />
+    </div>
   </div>
 </template>
 
 <script>
+import Tile from './Tile.vue'
+
 export default {
   name: "TileRow",
+  components: {
+    Tile
+  },
   props: {
-    text: String
+    tiles: Array
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.koaButton {
-  z-index: 1;
+$margin: 10px;
+.tileRow {
+  display: flex;
+  flex-flow: row wrap;
+  margin-left: -$margin;
+  margin-right: -$margin;
 }
-.main {
-  position: relative;
-  background-color: #10cfc9;
-  color: white;
-  font-size: 22px;
-  width: 250px;
-  height: 57px;
-  text-align: center;
-  padding-top: 17px;
+.tileContainer {
+  flex: 0 1 50%;
   box-sizing: border-box;
-  cursor: pointer;
-  transition: background-color 0.3s;
-  font-family: Poets;
-  &:after {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ffe904;
-    transform: translate3d(5px, 4px, 0);
-    z-index: -1 ;
-  }
-  &:hover, &:focus {
-    background-color: darken(#10cfc9, 10%);
-  }
+  padding: $margin;
 }
-
 </style>
