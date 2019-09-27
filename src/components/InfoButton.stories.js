6deb455b67bdfe4cce937c2ccc3fe319749d8ea3
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-09-27 17:37:02 +0200

    add InfoButton

---
+++ src/components/InfoButton.stories.js
@@ -0,0 +1,15 @@
+import { storiesOf } from '@storybook/vue';
+import InfoButton from './InfoButton';
+
+storiesOf('InfoButton', module)
+  .add('default', () => {
+    return {
+      components: { InfoButton },
+      template: `<div style="background-color: gray; padding: 50px;"><InfoButton :title="title" :text="text" :buttonText="buttonText" /></div>`,
+      data: () => ({
+        title: "Die Welt braucht dich wach",
+        text: "Mache mit einer Tasse koawach die Welt Schluck für Schluck fairer. Dank bester fair gehandelter Zutaten bleibt mehr bei den Bauern in Lateinamerika und mehr Genuss bei dir.",
+        buttonText: "Kauf dich wach",
+      }),
+    };
+  });
---
+++ src/components/InfoButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="infoButton">
+    <div class="info">
+      <h2>{{ title }}</h2>
+      <div class="text">{{ text }}</div>
+    </div>
+    <div class="button">
+      <KoaButton >{{ buttonText }}</KoaButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import KoaButton from './KoaButton.vue'
+
+export default {
+  name: "InfoButton",
+  components: {
+    KoaButton
+  },
+  props: {
+    title: String,
+    text: String,
+    buttonText: String
+  },
+  data() {
+    return {
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.infoButton {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+  max-width: 520px;
+  position: relative;
+}
+.info {
+  background-color: white;
+  padding: 10px 25px;
+  padding-bottom: 25px;
+  h2 {
+    margin: 0;
+  }
+}
+.button {
+  position: absolute;
+  right: -25px;
+  bottom: -44px;
+  z-index: 1;
+}
+</style>
