495daa329d9663e808895c01efb5bb6659f9b219
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-09-27 14:23:29 +0200

    add Container

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <slot>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Container",
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  max-width: 1020px;
+  margin: auto;
+  box-sizing: border-box;
+}
+</style>
