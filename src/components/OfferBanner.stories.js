4bf0c3c6704ef453982b36e5f90ca7d8695ae615
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-09-27 15:46:56 +0200

    add OfferBanner

---
+++ src/components/OfferBanner.stories.js
@@ -0,0 +1,25 @@
+import { storiesOf } from '@storybook/vue';
+import OfferBanner from './OfferBanner';
+
+storiesOf('OfferBanner', module)
+.add('default', () => {
+  return {
+    components: { OfferBanner },
+    template: `<OfferBanner text="schnapp dir pur: nur heute die 100 g packung für 2,99 euro kaufen!" />`,
+    data: () => ({ }),
+  };
+})
+.add('different color', () => {
+  return {
+    components: { OfferBanner },
+    template: `<OfferBanner backgroundColor="red" color="black" text="schnapp dir pur: nur heute die 100 g packung für 2,99 euro kaufen!" />`,
+    data: () => ({ }),
+  };
+})
+.add('no text', () => {
+  return {
+    components: { OfferBanner },
+    template: `<OfferBanner />`,
+    data: () => ({ }),
+  };
+});
---
+++ src/components/OfferBanner.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="offerBanner" :style="style">
+    <div class="main">
+      {{ text }}
+    </div>
+    <div class="close">
+      X
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OfferBanner",
+  props: {
+    text: {
+      type: String,
+      default: 'Advertisement text (please provide text via text prop).'
+    },
+    backgroundColor: {
+      type: String,
+      default: '#10cfc9'
+    },
+    color: {
+      type: String,
+      default: 'white'
+    }
+  },
+  computed: {
+    style() {
+      return {
+        backgroundColor: this.backgroundColor,
+        color: this.color
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.offerBanner {
+  width: 100%;
+  min-height: 50px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.text {
+  flex: 1;
+  margin: auto;
+  text-align: center;
+  width: 100%;
+}
+.close {
+  position: absolute;
+  top: 14px;
+  right: 14px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: all 0.3s;
+  &:hover {
+    font-size: 22px;
+    transform: rotate(90deg);
+  }
+}
+</style>
