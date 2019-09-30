2b5e2d0471348714205ca508830dc1461c055916
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-09-30 18:46:22 +0200

    add KoaHeadline

---
+++ src/components/atoms/KoaHeadline.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2 class="koaHeadline">
+    <slot>
+    </slot>
+  </h2>
+</template>
+
+<script>
+export default {
+  name: "KoaHeadline",
+}
+</script>
+
+<style lang="scss" scoped>
+.koaHeadline {
+  font-family: Poets;
+  font-size: 40px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.2;
+  letter-spacing: 1px;
+  text-align: left;
+  color: #000000;
+}
+</style>
