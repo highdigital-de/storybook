8922985b7cffd85b12e9a0f0d3f7381d06c39a66
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-08 17:14:55 +0200

    add ProductTile

---
+++ src/components/molecules/ProductTile.stories.js
@@ -0,0 +1,18 @@
+import { storiesOf } from '@storybook/vue';
+import ProductTile from './ProductTile';
+
+storiesOf('Design System|Molecules/ProductTile', module)
+  .add('default', () => {
+    return {
+      components: { ProductTile },
+      template: `<div style="width: 303px;"><ProductTile :product="product"></ProductTile></div>`,
+      data: () => ({
+        product: {
+          image: '/img/products/pur-mock-up-2.png',
+          title: 'koawach pur',
+          subtitle: 'purer Kakaogenuss',
+          price: 3.99
+        }
+      }),
+    };
+  });
---
+++ src/components/molecules/ProductTile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="productTile">
+    <img :src="product.image" class="productImage" />
+    <h3>{{ product.title }}</h3>
+    <div>{{ product.subtitle }}</div>
+    <h3 class="price">{{ product.price }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductTile",
+  props: {
+    product: Object
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.productTile {
+  text-align: center;
+}
+.price {
+  margin-top: 25px;
+}
+</style>
