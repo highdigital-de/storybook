debb2c04448ac365cbf6eecb305524fccfc2e103
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-09 12:10:04 +0200

    change Hero and scss

---
+++ src/components/molecules/Hero.stories.js
@@ -0,0 +1,74 @@
+import { storiesOf } from '@storybook/vue';
+import Hero from './Hero';
+
+storiesOf('Design System|Molecules/Hero', module)
+.add('default with', () => {
+  return {
+    components: { Hero },
+    template: `<Hero :items="this.items" :title="this.title" :text="this.text" :buttonText="this.buttonText" />`,
+    data: () => ({
+      items: [
+        {
+          title: "Die Welt braucht dich wach",
+          text: "Mache mit einer Tasse koawach die Welt Schluck für Schluck fairer. Dank bester fair gehandelter Zutaten bleibt mehr bei den",
+          buttonText: "Kauf dich wach",
+          background: "#eceff1",
+          image: "/img/cacao-bg.jpg"
+        },
+        {
+          title: "Anderer Titel",
+          buttonText: "Klick mal hier",
+          text: "Ganz anderer Text. Ganz anderer Text. Ganz anderer Text. Ganz anderer Text. Ganz anderer Text. Ganz anderer Text. ",
+          image: "/img/cacao-bg.jpg"
+        },
+        {
+          title: "Die Welt braucht dich wach",
+          buttonText: "Kauf dich wach",
+          text: "Mache mit einer Tasse koawach die Welt Schluck für Schluck fairer. Dank bester fair gehandelter Zutaten bleibt mehr bei den Bauern in Lateinamerika und mehr Genuss bei dir.",
+          background: "#fce4ec",
+          image: "/img/cacao-bg.jpg"
+        }
+      ],
+    }),
+  };
+})
+.add('no text', () => {
+  return {
+    components: { Hero },
+    template: `<Hero :items="this.items" :title="this.title" :text="this.text" :buttonText="this.buttonText" />`,
+    data: () => ({
+      items: [
+        {
+          background: "#eceff1",
+          image: "/img/cacao-bg.jpg"
+        },
+        {
+          title: "Anderer Titel",
+          buttonText: "Klick mal hier",
+          text: "Ganz anderer Text. Ganz anderer Text. Ganz anderer Text. Ganz anderer Text. Ganz anderer Text. Ganz anderer Text. ",
+          image: "/img/cacao-bg.jpg"
+        },
+        {
+          title: "Die Welt braucht dich wach",
+          buttonText: "Kauf dich wach",
+          text: "Mache mit einer Tasse koawach die Welt Schluck für Schluck fairer. Dank bester fair gehandelter Zutaten bleibt mehr bei den Bauern in Lateinamerika und mehr Genuss bei dir.",
+          background: "#fce4ec",
+          image: "/img/cacao-bg.jpg"
+        }
+      ],
+    }),
+  };
+})
+.add('no text no image no border', () => {
+  return {
+    components: { Hero },
+    template: `<Hero noBorder :items="this.items" :title="this.title" :text="this.text" :buttonText="this.buttonText" />`,
+    data: () => ({
+      items: [
+        {
+          background: "#eceff1"
+        }
+      ],
+    }),
+  };
+});
---
+++ src/components/molecules/Hero.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hero1Container" :class="{ border: !noBorder }">
+    <div class="hero1">
+      <SfHero :sliderOptions="{autoplay: false}">
+        <template>
+          <div v-for="(item, index) in items" :key="index">
+            <SfHeroItem
+              :buttonText="item.buttonText"
+              :background="item.background"
+              :image="item.image"
+              :class="index === 0 ? customClass1 : index === 1 ? customClass2 : customClass3"
+            >
+              <template #call-to-action>
+                <div class="margin">
+                  <InfoButton :title="item.title" :text="item.text" :buttonText="item.buttonText" />
+                </div>
+              </template>
+            </SfHeroItem>
+          </div>
+        </template>
+      </SfHero>
+    </div>
+  </div>
+</template>
+
+<script>
+import SfHero from '@storefront-ui/vue/src/components/organisms/SfHero/SfHero.vue'
+import SfButton from '@storefront-ui/vue/src/components/atoms/SfButton/SfButton.vue'
+import InfoButton from './../molecules/InfoButton.vue'
+
+export default {
+  name: "Hero1",
+  components: {
+    SfHero,
+    SfButton,
+    InfoButton
+  },
+  props: {
+    items: Array,
+    noBorder: Boolean
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../hdui.scss';
+
+.hero1Container {
+  &.border {
+    filter: drop-shadow(0px 4px 0px #10cfc9);
+  }
+}
+.hero1 {
+  @include clip-border;
+}
+.margin {
+  margin-top: 200px;
+  margin-bottom: 80px;
+}
+</style>
