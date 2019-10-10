45403d687a360c35aab8d97a586209edb1b8ff98
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-10 12:00:25 +0200

    change HeadingCTA to use BG on two lines

--- src/components/atoms/HeadingCTA.vue
+++ src/components/atoms/HeadingCTA.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="headingCTA">
-    <h2 class="heading">{{ heading }}</h2>
+    <div class="headingContainer">
+      <span class="heading">{{ heading }}</span>
+    </div>
     <div class="cta">{{ cta }}</div>
   </div>
 </template>
@@ -22,24 +24,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../../hdui.scss';
+
 .headingCTA {
   display: flex;
   flex-flow: column nowrap;
   align-items: flex-start;
 }
-.heading {
-  background-color: white;
-  padding: 8px 13px;
+.headingContainer {
   flex: 0 1 auto;
-  margin-bottom: 4px;
+  display: table;
+  padding: 8px 13px;
+  padding-bottom: 0;
+  margin-bottom: 2px;
+}
+.heading {
   font-size: 30px;
+  line-height: 43px;
+  background: repeating-linear-gradient(transparent, transparent 0px, $white 0px, $white 43px, transparent 43px, transparent 45px)
 }
 .cta {
   background-color: #ffe904;
   padding: 8px 13px;
   color: #1a1a18;
   flex: 0 1 auto;
-  margin-left: 17px;
+  margin-left: 23px;
   font-size: 18px;
   &:before {
     content: "> ";
