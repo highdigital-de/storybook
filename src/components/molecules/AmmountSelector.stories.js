aaeb7ab4d9ed8698cb649aa164c144186de8912e
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-14 16:36:15 +0200

    add notes to AmmountSelector story

--- src/components/molecules/AmmountSelector.stories.js
+++ src/components/molecules/AmmountSelector.stories.js
@@ -3,7 +3,7 @@ import AmmountSelector from './AmmountSelector';
 import { action } from '@storybook/addon-actions';
 
 storiesOf('Design System|Molecules/AmmountSelector', module)
-  .add('actions', () => {
+  .add('default (with bound actions)', () => {
     return {
       components: { AmmountSelector },
       template: `
@@ -17,6 +17,9 @@ storiesOf('Design System|Molecules/AmmountSelector', module)
         onIncrease: action('on-increase')
       }
     };
+  },
+  {
+    notes: 'The actions are emitted as events.',
   })
   .add('action bound', () => {
     return {
@@ -34,4 +37,7 @@ storiesOf('Design System|Molecules/AmmountSelector', module)
         onIncrease() {this.ammount += 1}
       }
     };
+  },
+  {
+    notes: 'Action are bound to storybook data. The component is a non-functional component that emits actions and does not calculate or safe state.',
   });
