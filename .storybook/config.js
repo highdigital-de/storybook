d127570c5dbab50f2bd6f871732c5c291291ca01
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-16 14:58:06 +0200

    add viewports to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
 import { configure } from '@storybook/vue';
+import { addParameters } from '@storybook/vue';
+
+addParameters({
+});
 
 const req = require.context('../src', true, /\.stories.js$/);
 function loadStories() {
