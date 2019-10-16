4ac76d31badb43b7b5a9179e17bd4c629d57886c
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-16 14:01:04 +0200

    add AmmountSelector and FilterSelector to ProductH

--- src/components/organisms/ProductHero.stories.js
+++ src/components/organisms/ProductHero.stories.js
@@ -5,13 +5,19 @@ storiesOf('Design System|Organisms/ProductHero', module)
   .add('default Vanille', () => {
     return {
       components: { ProductHero },
-      template: `<ProductHero :colors="this.colors"></ProductHero>`,
+      template: `<ProductHero :colors="this.colors" :sizeOptions="this.sizeOptions" :selectedSize="this.selectedSize"></ProductHero>`,
       data: () => ({
         colors: {
           background: '#93c0e9',
           color: '#ffffff',
           koaButton: 'yellow'
-        }
+        },
+        sizeOptions: [
+          { label: '100 g', value: '100' },
+          { label: '220 g', value: '220' },
+          { label: '500 g', value: '500' }
+        ],
+        selectedSize: 220
       }),
     };
   });
--- src/components/organisms/ProductHero.vue
+++ src/components/organisms/ProductHero.vue
@@ -8,16 +8,26 @@
         </div>
         <div class="column is-5">
           <KoaHeadline>typ vanille</KoaHeadline>
+          <RatingLink :score="4" :count="11" />
           <h3>verführerisch vanillig enthält koffeinhaltiges guarana super lecker</h3>
           <KoaHeadline>3,59</KoaHeadline>
           <div>
             <Batch>-10%</Batch>statt 3,99€ pro 100g
           </div>
+          <div class="columns">
+            <div class="column is-one-third">
+              <h3>Menge</h3>
+              <AmmountSelector :ammount="1" />
+            </div>
+            <div class="column is-two-thirds">
+              <h3>Größe</h3>
+              <FilterSelector :options="sizeOptions" :selected="selectedSize" :color="colors.color" :backgroundColor="colors.background" />
+            </div>
+          </div>
           <p>
             inkl. 7 % MwSt. zzgl. Versand<br />
             Lieferzeit: ca. 1-3 Tage nach DE
           </p>
-          <Whitespace />
           <KoaButton :variant="colors.koaButton" >in den Warencorb</KoaButton>
         </div>
         <div class="is-1">
@@ -35,6 +45,9 @@ import KoaHeadline from './../atoms/KoaHeadline.vue';
 import Whitespace from './../atoms/Whitespace.vue';
 import Batch from './../atoms/Batch.vue';
 import KoaButton from './../atoms/KoaButton.vue';
+import RatingLink from './../molecules/RatingLink.vue';
+import AmmountSelector from './../molecules/AmmountSelector.vue';
+import FilterSelector from './../molecules/FilterSelector.vue';
 
 export default {
   name: "ProductHero",
@@ -47,6 +60,15 @@ export default {
         koaButton: 'yellow'
       }
     },
+    sizeOptions: {
+      type: Object,
+      default: [
+        {label: '100 g', value: '100'},
+        {label: '220 g', value: '220'},
+        {label: '500 g', value: '500'}
+      ]
+    },
+    selectedSize: Number
   },
   components: {
     Container,
@@ -54,7 +76,10 @@ export default {
     KoaTitle,
     KoaHeadline,
     Batch,
-    KoaButton
+    KoaButton,
+    RatingLink,
+    AmmountSelector,
+    FilterSelector
   },
   computed: {
     heroStyle() {
