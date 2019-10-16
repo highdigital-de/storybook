b505dd0aaebe1b365f132528ad7dfbfef2d9cb98
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-16 14:00:07 +0200

    change color of Rating

--- src/components/atoms/Rating.vue
+++ src/components/atoms/Rating.vue
@@ -3,7 +3,7 @@
     <div class="starContainer" v-for="index in Math.abs(score)">
       <svg height="13.23" viewBox="0 0 13.912 13.23" width="13.912" xmlns="http://www.w3.org/2000/svg"><path d="m204.883 663.8 4.3 3.3-1.815-5.11 4.472-3.068-5.421.147-1.535-5.2-1.535 5.2-5.421-.147 4.472 3.068-1.815 5.11z" fill="#ffe904" transform="translate(-197.928 -653.874)"/></svg>
     </div><div class="starContainer" v-for="index in Math.abs(max - score)">
-      <svg height="13.23" viewBox="0 0 13.912 13.23" width="13.912" xmlns="http://www.w3.org/2000/svg"><path d="m204.883 663.8 4.3 3.3-1.815-5.11 4.472-3.068-5.421.147-1.535-5.2-1.535 5.2-5.421-.147 4.472 3.068-1.815 5.11z" fill="#000" transform="translate(-197.928 -653.874)"/></svg>
+      <svg height="13.23" viewBox="0 0 13.912 13.23" width="13.912" xmlns="http://www.w3.org/2000/svg"><path d="m204.883 663.8 4.3 3.3-1.815-5.11 4.472-3.068-5.421.147-1.535-5.2-1.535 5.2-5.421-.147 4.472 3.068-1.815 5.11z" fill="#ffffff" transform="translate(-197.928 -653.874)"/></svg>
     </div>
   </div>
 </template>
