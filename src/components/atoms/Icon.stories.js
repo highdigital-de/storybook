4a52393929e2e7e6c2ed696b7b0fb2944045230c
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-17 17:37:57 +0200

    add Icon

---
+++ src/components/atoms/Icon.stories.js
@@ -0,0 +1,22 @@
+import { storiesOf } from '@storybook/vue';
+import Icon from './Icon';
+
+storiesOf('Design System|Atoms/Icon', module)
+.add('person', () => {
+  return {
+    components: { Icon },
+    template: `<div style="background-color: gray;">
+    <Icon icon="person" />
+    </div>`,
+    data: () => ({ }),
+  };
+})
+.add('no data', () => {
+  return {
+    components: { Icon },
+    template: `<div style="background-color: gray;">
+    <Icon />
+    </div>`,
+    data: () => ({ }),
+  };
+});
---
+++ src/components/atoms/Icon.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="icon" :style="style">
+    
+  </div>
+</template>
+
+<script>
+import Container from './../atoms/Container.vue'
+
+export default {
+  name: "Icon",
+  props: {
+    icon: String
+  },
+  data() {
+    return {
+      dir: '/img/icons/ui/',
+      icons: {
+        'person': 'person_white.svg',
+        'cart': 'cart_white.svg',
+        'hamburger': 'hamburger_white.svg',
+      }
+    }
+  },
+  computed: {
+    style() {
+      return {
+        backgroundImage: 'url(' + this.dir + this.icons[this.icon] + ')'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../hdui.scss';
+
+.icon {
+  width: $icon-width;
+  height: $icon-height;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: contain;
+  @media only screen and (min-width: $breakpoint-mobile-landscape) {
+  
+  }
+}
+</style>
