1a545477915c7821e029f5e6adbe763fb4c35419
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-22 17:32:48 +0200

    put all CategoriePages in one Story

--- src/components/pages/CategoryPage.stories.js
+++ src/components/pages/CategoryPage.stories.js
@@ -1,11 +1,43 @@
 import { storiesOf } from '@storybook/vue';
 import CategoryPage from './CategoryPage';
+import DrinkPage from './DrinkPage';
+import EquipmentPage from './EquipmentPage';
+import OfferPage from './OfferPage';
+import SetPage from './SetPage';
 
 storiesOf('Design System|Pages/CategoryPage', module)
-  .add('default', () => {
-    return {
-      components: { CategoryPage },
-      template: `<CategoryPage></CategoryPage>`,
-      data: () => ({ }),
-    };
-  });
+.add('PowerOverview', () => {
+  return {
+    components: { CategoryPage },
+    template: `<CategoryPage></CategoryPage>`,
+    data: () => ({ }),
+  };
+})
+.add('DrinkPage', () => {
+  return {
+    components: { DrinkPage },
+    template: `<DrinkPage></DrinkPage>`,
+    data: () => ({ }),
+  };
+})
+.add('EquipmentPage', () => {
+  return {
+    components: { EquipmentPage },
+    template: `<EquipmentPage></EquipmentPage>`,
+    data: () => ({ }),
+  };
+})
+.add('OfferPage', () => {
+  return {
+    components: { OfferPage },
+    template: `<OfferPage></OfferPage>`,
+    data: () => ({ }),
+  };
+})
+.add('SetPage', () => {
+  return {
+    components: { SetPage },
+    template: `<SetPage></SetPage>`,
+    data: () => ({ }),
+  };
+});
