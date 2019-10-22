f2eaed49b931dd8c6eb075607ef2072419f7f9c7
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-10-22 15:20:31 +0200

    add EquipmentPage

---
+++ src/components/pages/EquipmentPage.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from '@storybook/vue';
+import EquipmentPage from './EquipmentPage';
+
+storiesOf('Design System|Pages/EquipmentPage', module)
+  .add('default', () => {
+    return {
+      components: { EquipmentPage
+     },
+      template: `<EquipmentPage
+    ></EquipmentPage
+    >`,
+      data: () => ({ }),
+    };
+  });
