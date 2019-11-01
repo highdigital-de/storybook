0bff2e7cb848a5d9fbf33d416135eac5f32e9746
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-11-01 17:30:38 +0100

    remove attr from SfHeroItem in Hero

--- src/components/molecules/Hero.vue
+++ src/components/molecules/Hero.vue
@@ -5,9 +5,6 @@
         <template>
           <div v-for="(item, index) in items" :key="index">
             <SfHeroItem
-              :buttonText="item.buttonText"
-              :background="item.background"
-              :image="item.image"
               :class="index === 0 ? customClass1 : index === 1 ? customClass2 : customClass3"
             >
               <template #call-to-action>
