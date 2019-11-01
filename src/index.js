e4359ff8166fceafb7ef7118987e5e48d19e8fdc
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-11-01 15:02:05 +0100

    add main entry file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import KoaButton from './components/atoms/KoaButton.vue'
+import Carousel from './components/organisms/Carousel.vue'
+import Arrow from '@storefront-ui/vue/components/atoms/SfArrow'
+
+export {
+  KoaButton,
+  Carousel,
+  Arrow
+}
